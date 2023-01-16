e3c7635ded8a2c0e38e13dc859dce6fa43c57fa6
Ramzi-dev611 <ramzi.latrous@insat.ucar.tn>
2023-01-16 19:00:19 +0100

    added microstack for the application deployment (chart path needing fix)

---
+++ application-install-charts/main.tf
@@ -0,0 +1,15 @@
+data "terraform_remote_state" "aks" {
+  backend = "azurerm"
+
+  config = {
+    resource_group_name  = "terraform_backend_rg"
+    container_name       = "backend"
+    storage_account_name = "storebackend124"
+    key                  = "kubernetes-stack.json"
+  }
+}
+
+resource "helm_release" "application" {
+    name = "application"
+    chart = "../../barber-shop/charts"
+}
