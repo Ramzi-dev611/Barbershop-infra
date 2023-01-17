011107bb5ee036825973003c8cc420d72eea05d5
Ramzi-dev611 <ramzi.latrous@insat.ucar.tn>
2023-01-17 17:36:02 +0100

    fix: fixes in the application stack

--- application-install-charts/main.tf
+++ application-install-charts/main.tf
@@ -9,7 +9,77 @@ data "terraform_remote_state" "aks" {
   }
 }
 
+data "terraform_remote_state" "db" {
+  backend = "azurerm"
+
+  config = {
+    resource_group_name  = "terraform_backend_rg"
+    container_name       = "backend"
+    storage_account_name = "storebackend124"
+    key                  = "database-stack.json"
+  }
+}
+
 resource "helm_release" "application" {
     name = "application"
-    chart = "../../barber-shop/charts"
+    chart = "../charts/application"
+    set {
+      name = "gateway.deployment.jwt"
+      value = var.jwt_secret
+    }
+    set {
+      name = "authentication.deployment.jwt"
+      value = var.jwt_secret
+    }
+
+    set {
+      name = "authentication.deplyment.DB_HOST"
+      value = data.terraform_remote_state.db.outputs.database-service-name
+    }
+
+    set {
+      name = "authentication.deplyment.DB_PORT"
+      value = data.terraform_remote_state.db.outputs.database-port
+    }
+
+    set {
+      name = "authentication.deplyment.DB_NAME"
+      value = data.terraform_remote_state.db.outputs.database-name
+    }
+
+    set {
+      name = "authentication.deplyment.DB_USER"
+      value = data.terraform_remote_state.db.outputs.database-username
+    }
+
+    set {
+      name = "authentication.deplyment.DB_PASSWD"
+      value = data.terraform_remote_state.db.outputs.database-password
+    }
+
+    set {
+      name = "posts.deplyment.DB_HOST"
+      value = data.terraform_remote_state.db.outputs.database-service-name
+    }
+
+    set {
+      name = "posts.deplyment.DB_PORT"
+      value = data.terraform_remote_state.db.outputs.database-port
+    }
+
+    set {
+      name = "posts.deplyment.DB_NAME"
+      value = data.terraform_remote_state.db.outputs.database-name
+    }
+
+    set {
+      name = "posts.deplyment.DB_USER"
+      value = data.terraform_remote_state.db.outputs.database-username
+    }
+
+    set {
+      name = "posts.deplyment.DB_PASSWD"
+      value = data.terraform_remote_state.db.outputs.database-password
+    }
+
 }
---
+++ application-install-charts/variables.tf
@@ -0,0 +1,4 @@
+variable "jwt_secret" {
+  type = string
+  description = "jwt secret key"
+}
